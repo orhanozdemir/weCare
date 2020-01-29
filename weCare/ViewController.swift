eb9cb9cdeb3629bafd8dd31dcee3e1dda247e305
Orhan Özdemir <ozdemirorhan95@gmail.com>
2020-01-29 10:39:50 +0300

    Initial Commit

---
+++ weCare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weCare
+//
+//  Created by Orhan Özdemir on 29.01.2020.
+//  Copyright © 2020 PRS Kozmetik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
